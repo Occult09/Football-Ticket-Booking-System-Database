5f32c12bbbc936d8cfe1ddc275eb687dab1c8169
Maheer Musfiq <maheermusfiq221@gmail.com>
2026-07-09 19:52:42 +0600

    retrieved match booking details along with the users fullname and the scheduled match fixture teams

--- query.sql
+++ query.sql
@@ -109,3 +109,16 @@ from
   bookings
 where
   payment_status is null;
+
+-- =========================================================================
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+-- =========================================================================
+select
+  b.booking_id,
+  u.full_name,
+  m.fixture,
+  b.total_cost
+from
+  users as u
+  join bookings as b on u.user_id = b.user_id
+  join matches as m on m.match_id = b.match_id;
