bef4c5977526b71a7a9e002a42b45b184f18b3df
Maheer Musfiq <maheermusfiq221@gmail.com>
2026-07-09 20:03:42 +0600

    retrieved all ticket bookings where the total cost is higher than the average cost of all ticket bookings

--- query.sql
+++ query.sql
@@ -133,3 +133,20 @@ select
 from
   users as u
   left join bookings as b on u.user_id = b.user_id;
+
+-- =========================================================================
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+-- =========================================================================
+select
+  booking_id,
+  match_id,
+  total_cost
+from
+  bookings
+where
+  total_cost > (
+    select
+      avg(total_cost)
+    from
+      bookings
+  );
